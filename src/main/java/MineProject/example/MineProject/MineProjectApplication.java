af1259593dcee591a8b0d1a1431cffa20e9c4d8d
sakthi59 <sakthisaravana59@gmail.com>
2025-03-22 05:55:46 +0530

    initial commit

---
+++ src/main/java/MineProject/example/MineProject/MineProjectApplication.java
@@ -0,0 +1,13 @@
+package MineProject.example.MineProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MineProject/example/MineProject/MineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package MineProject.example.MineProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
